98e4768f717944f81915e06e48e3813d214b7d52
Manideep Gujjari <manideep.gujjari@gmail.com>
2025-01-26 08:52:35 +0530

    lint

--- tests/sample.py
+++ tests/sample.py
@@ -1,13 +1,16 @@
 import unittest
 
+
 def add(x, y):
     return x + y
+
 
 class MyTestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
